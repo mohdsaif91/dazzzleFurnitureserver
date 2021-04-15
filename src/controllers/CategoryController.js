45e108092a54be27772b216a5ba29b09f1a08aee
Mohd Saif Farooqui <stack9167@gmail.com>
2021-04-15 15:34:42 +0530

    changes in package

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -3,8 +3,10 @@ const categorySchema = require("../models/category");
 const getCountCategory = async (req, res) => {
   try {
     const category = await categorySchema.find({});
+    console.log(category);
     res.status(200).json({ category });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error });
   }
 };
